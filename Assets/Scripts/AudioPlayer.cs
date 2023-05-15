a5e1ef7a7213f6f9574f887bbf449a968daa19ac
Jacob-Judge-Student <jajuf70@student.oaisd.org>
2023-05-15 10:38:18 -0400

    got rid of the gun for shops, attempted to add laser sound effect
    added new script, AudioPlayer, disabled gun for shops, wouldnt make sense to shoot it there

---
+++ Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+    [Header("Shooting")]
+    [SerializeField] AudioClip shootingClip;
+    [SerializeField][Range(0f, 1f)] float shootingVolume = 1f;
+
+
+   
+    static AudioPlayer instance;
+
+    public AudioPlayer GetInstance()
+    {
+        return instance;
+    }
+
+
+
+    void Awake()
+    {
+        ManageSingleton();
+    }
+
+    void ManageSingleton()
+    {
+
+        if (instance != null)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void PlayShootingClip()
+    {
+        PlayClip(shootingClip, shootingVolume);
+    }
+
+    void PlayClip(AudioClip clip, float volume)
+    {
+        
+    }
+}

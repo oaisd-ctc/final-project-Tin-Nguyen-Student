3f00e71cdaa1286153be925222bea1c2960832dc
Tin-Nguyen-Student <ting566@student.oaisd.org>
2023-05-17 21:51:15 -0400

    Saved

---
+++ Assets/Scripts/GameOverScreen.cs
@@ -0,0 +1,26 @@
+using System.Net.Sockets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameOverScreen : MonoBehaviour
+{
+    public Text pointsText;
+
+    public void Setup(int score) 
+    {
+        gameObject.SetActive(true);
+        pointsText.text = score.ToString() + " POINTS";
+    }
+    public void RestartButton()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void ExitButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
